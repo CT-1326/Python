035cc8afed2aeb2c1727cab55854985ea4d6c4e8
DESKTOP-4BKRPIN\withnetworks <smw1326@withnetworks.com>
2023-02-08 10:46:58 +0900

    파일 변동

--- flaskAPI/index.py
+++ flaskAPI/index.py
@@ -1,7 +1,11 @@
+import base64
+
 from flask import Flask
 from flask import jsonify
+from flask import request
+from flask import Response
 
-app = Flask(__name__)
+app = Flask(__name__)  # flask 앱 인스턴스화
 
 
 @app.route('/')
@@ -23,6 +27,26 @@ def get_user_info(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    ADMIN_NAME = 'root'
+    ADMIN_PASSWORD = '12345'
+    # 헤더에 로그인 정보를 담아 전송하는만큼 디코딩 진행 및 관련 변수에 초기화
+    auth = request.headers.get('Authorization')
+    auth = auth.split()[-1]
+    auth = base64.b64decode(auth).decode('UTF-8')
+    name, password = auth.split(':')
+
+    if name == ADMIN_NAME and password == ADMIN_PASSWORD:
+        response = {
+            'name': name,
+            'message': 'ok'
+        }
+        return jsonify(response), 200
+
+    return Response('Unauthorized', 401)
 
 
 if __name__ == '__main__':
